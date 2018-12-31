13f6a8453210f3e3e1f77c5e4b03ccbc88f15422
吉翔 <jx4554@gmail.com>
2018-12-31 17:20:21 +0800

    Config rails_event_store

--- config/initializers/rails_event_store.rb
+++ config/initializers/rails_event_store.rb
@@ -1,3 +1,16 @@
+require 'arkency/command_bus'
+
 Rails.configuration.to_prepare do
   Rails.configuration.event_store = RailsEventStore::Client.new
+  Rails.configuration.command_bus = Arkency::CommandBus.new
+  
+  Rails.configuration.event_store.tap do |store|
+    store.subscribe(Orders::OnItemAddedToBasket, to: [Ordering::ItemAddedToBasket])
+    store.subscribe(Orders::OnOrderSubmitted, to: [Ordering::OrderSubmitted])
+  end
+
+  Rails.configuration.command_bus.tap do |bus|
+    bus.register(Ordering::AddItemToBasket, Ordering::OnAddItemToBasket.new)
+    bus.register(Ordering::SubmitOrder, Ordering::OnSubmitOrder.new(number_generator: Rails.configuration.number_generator))
+  end
 end
